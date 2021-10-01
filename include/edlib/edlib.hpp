1d8b8a9e231ab28b7efdbf531de1b25cc1df74ea
Chae-Yeun Park <kaeri17@gmail.com>
2021-10-01 16:15:42 +0200

    edlib.hpp now includes all basis

--- include/edlib/edlib.hpp
+++ include/edlib/edlib.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "Basis/BasisFull.hpp"
+#include "Basis/BasisFullZ2.hpp"
+
 #include "Basis/Basis1D.hpp"
 #include "Basis/Basis1DZ2.hpp"
+
+#include "Basis/Basis2D.hpp"
+#include "Basis/Basis2DZ2.hpp"
 
 #include "EDP/LocalHamiltonian.hpp"
 #include "EDP/ConstructSparseMat.hpp"
